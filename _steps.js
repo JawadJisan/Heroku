41a0359c61793a16d44c5590adff9f9d177fdfcc
zahidmiran <zahidmiron599@gmail.com>
2022-04-26 12:12:02 +0600

    steps added

---
+++ _steps.js
@@ -0,0 +1,15 @@
+/* 
+-----------
+one time
+----------
+1. create herocu account
+2. verify email
+3. install heroku cli
+4. heroku login
+
+------------
+for each project one time
+-------------
+1. heroku creat
+
+*/
